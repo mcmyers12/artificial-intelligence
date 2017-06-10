3c9e04c965c1130cbac4ab50e1dbb1c09ebbf3c0
mcmyers12 <mirandacmyers12@gmail.com>
2017-06-10 14:53:24 -0400

    Print execution of algorithm

--- successive-elimination-of-dominated-strategies/module-02-programming.py
+++ successive-elimination-of-dominated-strategies/module-02-programming.py
@@ -19,9 +19,41 @@ def remove_column(game, column):
     return game
 
 
+def flip_game(game):
+    num_rows = len(game)
+    row_length = len(game[0])
+    
+    flipped_game = []
+    for i in range(row_length):
+        column = []
+        for j in range(num_rows):
+            column.append(game[j][i])
+        flipped_game.append(column)
+            
+    return flipped_game
+
+
+def switch_player(player):
+    if player == 0:
+        return 1
+    else:
+        return 0
+
+
+def check_for_one_cell(game):
+    if len(game) == 1 and len(game[0]) == 1:
+        return True
+    return False
+    
+def print_game(game):
+    print 'game:'
+    for row in game:
+        print row
+
+
 def remove_strongly_dominated_strategy_player_1(game):
-    print 'game: ', game
-    print 'length: ', len(game), '\n\n'
+    print '\n\nremove_strongly_dominated_strategy_player_1'
+    print_game(game)
         
     for i in range(len(game) - 1):
         strategy1 = game[i]
@@ -54,9 +86,12 @@ def remove_strongly_dominated_strategy_player_1(game):
     print 'no dominated strategy'
     return game
 
-#A flipped game must be passed in here
-def remove_strongly_dominated_strategy_player_2(game):
 
+
+def remove_strongly_dominated_strategy_player_2(game):
+    print '\n\nremove_strongly_dominated_strategy_player_2'
+    print_game(game)
+    
     flipped_game = flip_game(game)
     for i in range(len(flipped_game) - 1):
         strategy1 = flipped_game[i]
@@ -89,39 +124,11 @@ def remove_strongly_dominated_strategy_player_2(game):
     print 'no dominated strategy'
     return game  
 
-def flip_game(game):
-    num_rows = len(game)
-    row_length = len(game[0])
-    
-    flipped_game = []
-    for i in range(row_length):
-        column = []
-        for j in range(num_rows):
-            column.append(game[j][i])
-        flipped_game.append(column)
-        
-    print '\nflipped game: ', flipped_game
-    
-    return flipped_game
-
-new_game = remove_strongly_dominated_strategy_player_2(prisoners_dilemma)
-print '\n\nnew_game'
-for row in new_game:
-    print row
-
-
-def switch_player(player):
-    if player == 0:
-        return 1
-    else:
-        return 0
-
-
 
 def solve_game(game, weak=False):
     player = 0
     if not weak:
-        while len(game) > 1:
+        while not check_for_one_cell(game):
             remove_strongly_dominated_strategy_player_1(game)
             player = switch_player(player)
             remove_strongly_dominated_strategy_player_2(game)
@@ -129,7 +136,7 @@ def solve_game(game, weak=False):
     print '\n\nNash Equilibrium:\n', game
 
     
-solve_game(prisoners_dilemma)
+solve_game(bars)
 
 
 
