e3ab790f4ea0f365e2f143cee486f43990f41372
mcmyers12 <mirandacmyers12@gmail.com>
2017-07-26 20:28:14 -0400

    Possibly generates the initial neural network

--- artificial-neural-networks/artificial-neural-networks.py
+++ artificial-neural-networks/artificial-neural-networks.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import sys
+import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
 
 clean_data = {
     "plains": [
@@ -48,52 +50,6 @@ def blur( data):
     return noisy_readings + [data[-1]]
 
 
-
-def generate_hills(data, n, data_out):
-    y = [1, 0, 0, 0]
-
-    hill_index = 0
-    for i in range(n):
-        blurred_hill = blur(clean_data["hills"][hill_index])
-        blurred_hill[-1] = y                        #TODO: make sure I don't need to make a deep copy of this
-        data_out.append(blurred_hill)
-
-        if hill_index < 3:
-            hill_index += 1
-        else:
-            hill_index = 0
-
-
-def generate_swamps(data, n, data_out):
-    y = [0, 1, 0, 0]
-
-    swamp_index = 0
-    for i in range(n):
-        blurred_hill = blur(clean_data["hills"][hill_index])
-        blurred_hill[-1] = y                        #TODO: make sure I don't need to make a deep copy of this
-        data_out.append(blurred_hill)
-
-        if hill_index < 3:
-            hill_index += 1
-        else:
-            hill_index = 0
-            
-
-def generate_forests(data, n, data_out):
-    y = [0, 0, 1, 0]
-
-    swamp_index = 0
-    for i in range(n):
-        blurred_hill = blur(clean_data["hills"][hill_index])
-        blurred_hill[-1] = y                        #TODO: make sure I don't need to make a deep copy of this
-        data_out.append(blurred_hill)
-
-        if hill_index < 3:
-            hill_index += 1
-        else:
-            hill_index = 0
-            
-
 def generate_labeled_data(data, n, label, one_hot_index, data_out):
     y = [0, 0, 0, 0]
     y[one_hot_index] = 1
@@ -104,14 +60,11 @@ def generate_labeled_data(data, n, label, one_hot_index, data_out):
         blurred_data = blur(data[label][index])
         blurred_data[-1] = y                        #TODO: make sure I don't need to make a deep copy of this
         data_out.append(blurred_data)
-        
-        #TODO remove
-        data_out.append(data[label][index])
 
         if index < loop:
             index += 1
         else:
-            index = 0
+            index = 0          
 
 
 def generate_data(data, n):
@@ -125,24 +78,82 @@ def generate_data(data, n):
     return data_out
 
 
-results = generate_data( clean_data, 10)
-for result in results:
+def create_network(data, hidden_nodes, output_nodes):
+    num_thetas = len(data[0])                                       #TODO is this right???
+    network = { 'hidden': [], 'output': [] }
+    
+    for i in range(hidden_nodes):
+        hidden_node_thetas = [random.uniform(0, 1) for x in range(num_thetas)]
+        network['hidden'].append(hidden_node_thetas)
+        
+    for i in range(output_nodes):
+        output_node_thetas = [random.uniform(0, 1) for x in range(num_thetas)]
+        network['output'].append(output_node_thetas)
+        
+        
+    return network
+
+
+
+def dot_product(thetas, xs):
+    z = 0
+    if len(thetas) != len(xs):
+        print '\n\n\nthetas length different than xs\n\n\n'
+
+    for i in range(len(thetas)):
+        z += thetas[i] * xs[i]
+
+    return z
+
+
+def calculate_yhat(thetas, xs):
+    z = dot_product(thetas, xs)
+    yhat = 1 / (1 + math.e ** (-z))
+
+    if yhat < 0 or yhat > 1:
+        print '\n\n\nyhat not in range 0, 1\n\n\n'
+
+    return yhat
+
+
+def calculate_delta_o(y, yhat):
+    delta_o = yhat * (1 - yhat) * (y - yhat)
+    
+    return delta_o    
+
+# Use `learn_model` to learn a ANN model for classifying sensor images as hills, swamps, plains or forest. 
+# The hidden layer will be one vector of thetas for each hidden node
+# The output layer will be one vector of thetas for each output (4 outputs)
+def learn_model(data, hidden_nodes, verbose=False):
+    network = create_network(data, hidden_nodes, 4)
+    #initialize thetas
+    #previous_error = 0.0
+    #while abs(current_error - previous_error) > epsilon
+
+    pp.pprint(network)
+
+
+
+
+
+
+
+
+
+
+
+train_data = generate_data( clean_data, 5)
+for result in train_data:
     for x in result:
         if type(x) == float:
             sys.stdout.write(str(round(x, 2)) + '\t')
         else:
             sys.stdout.write(str(x) + '\t')
     print
-
-'''
-# Use `learn_model` to learn a ANN model for classifying sensor images as hills, swamps, plains or forest. Use your `generate_data` function to generate a training set with 100 examples for each. **Set Verbose to True**
-def learn_model( data, hidden_nodes, verbose=False):
-    pass
-
-train_data = generate_data( clean_data, 100)
+print
 model = learn_model( train_data, 2, True)
 
-
+'''
 # Use `generate_data` to generate 100 blurred examples of each terrain and use this as your test data. Print out the first 10 results, one per line.
 test_data = generate_data( clean_data, 100)
 
