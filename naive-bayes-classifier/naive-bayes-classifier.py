54f449ea6b5144d26c8893a9759dc40f864fe77f
mcmyers12 <mirandacmyers12@gmail.com>
2017-08-12 14:24:18 -0400

    Working naive bayes classifier

--- naive-bayes-classifier/naive-bayes-classifier.py
+++ naive-bayes-classifier/naive-bayes-classifier.py
@@ -19,6 +19,20 @@ def create_train_test_sets(data):
     train_set = data[split_point:]
     
     return train_set, test_set
+
+
+def get_class_label_counts(data):
+    p_count = 1.0
+    e_count = 1.0
+    for row in data:
+        if row[0] == 'p':
+            p_count += 1.0
+        elif row[0] == 'e':
+            e_count += 1.0
+    
+    print
+    print 'p_count', p_count, 'e_count', e_count
+    return p_count, e_count
     
 
 def get_probability_counts(data):
@@ -39,25 +53,31 @@ def get_probability_counts(data):
                     probability_counts[i][attribute_value]['p'] += 1.0
                 elif row[0] == 'e':
                     probability_counts[i][attribute_value]['e'] += 1.0
+                else:
+                    print 'not p or e'
+                    print
+                    print 
+                    row[0]
+                    print
+                    print
+                    print
     
     pp.pprint(probability_counts)            
     return probability_counts
 
 
-def get_class_label_counts(data):
-    p_count = 1.0
-    e_count = 1.0
-    for row in data:
-        if row[0] == 'p':
-            p_count += 1.0
-        elif row[0] == 'e':
-            e_count += 1.0
+def get_class_probabilities(p_count, e_count):
+    class_probabilities = { 'p' : {}, 'e' : {} }
+    p_count = p_count - 1.0
+    e_count = e_count - 1.0
+    total = p_count + e_count
     
-    print
-    print 'p_count', p_count, 'e_count', e_count
-    return p_count, e_count
-
-        
+    class_probabilities['p']['probability'] = p_count / total
+    class_probabilities['e']['probability'] = e_count / total
+    class_probabilities['p']['count'] = p_count 
+    class_probabilities['e']['count'] = e_count 
+    
+    return class_probabilities
 
 
 def learn(data):
@@ -75,13 +95,17 @@ def learn(data):
 
 def calculate_probability(probabilities, instance, label, class_probabilities):
     probability = 1
-    
     for attribute_index in range(len(instance) - 1):
         attribute_index += 1                                #skip class label
         attribute_value = instance[attribute_index]
-        probability *= probabilities[attribute_index][attribute_value][label]
         
-    probability *= class_probabilities[label]
+        if attribute_value in probabilities[attribute_index]:
+            probability *= probabilities[attribute_index][attribute_value][label]
+        
+        else:
+            probability *= 1 / class_probabilities[label]['count']  #If some combination wasn't in the training data, do +1 smoothing
+        
+    probability *= class_probabilities[label]['probability']
     
     return probability
 
@@ -89,8 +113,8 @@ def calculate_probability(probabilities, instance, label, class_probabilities):
 def normalize(results):
     denominator = results['p'] + results['e']
     
-    results['p'] = results['p'] / denominator
-    results['e'] = results['e'] / denominator
+    results['p'] /= denominator
+    results['e'] /= denominator
     
     return results
 
@@ -108,19 +132,6 @@ def classify_instance(probabilities, instance, class_probabilities):
         return ('e', results['e'])
     
 
-
-def get_class_probabilities(p_count, e_count):
-    class_probabilities = {}
-    p_count = p_count - 1.0
-    e_count = e_count - 1.0
-    total = p_count + e_count
-    
-    class_probabilities['p'] = p_count / total
-    class_probabilities['e'] = e_count / total
-    
-    return class_probabilities
-
-
 #Returns a list of tuples: each is a class and the normalized probability of that class
 #sorted in descending order
 #[("e", 0.98), ("p", 0.02)]
@@ -132,19 +143,45 @@ def classify(probabilities, instances):
     for instance in instances:
         classifications.append(classify_instance(probabilities, instance, class_probabilities))
     
-    return sorted(classifications, key = lambda x: x[1])
-    
+    #return sorted(classifications, key = lambda x: x[1])
+    return classifications
+
 
 #Uses the error rate    
-def evaluate():
-    pass
+def evaluate(data, classifications):
+    errors = 0.0
+    for i in range(len(data)):
+        if classifications[i][0] != data[i][0]:
+            errors += 1
+    
+    print 'errors ', errors
+    print 'len(data) ', len(data)  
+    error_rate = errors / len(data)
+    return error_rate
 
 
 data = read_csv('agaricus-lepiota.data')
-probabilities = learn(data)
+set1, set2 = create_train_test_sets(data)
 
-classifications = classify(probabilities, data) #TODO use test data
-pp.pprint(classifications)
+
+probabilities = learn(set1)
+classifications = classify(probabilities, set2) 
+
+error_rate = evaluate(set2, classifications)
+print 'error_rate ', error_rate
+print
+print
+print
+
+probabilities = learn(set2)
+classifications = classify(probabilities, set1) 
+
+error_rate = evaluate(set1, classifications)
+print 'error_rate ', error_rate
+print
+print
+print
+
 
 
 
